e695a6d3710c9835805012111ac1ebd9567f4071
lelouvincx <dinhminhchinh3357@gmail.com>
2023-12-18 22:23:59 +0700

    Update code location
    Movies asset
    Pretrained Comments asset

---
+++ pipeline_data_platform/data_platform/assets/constants.py
@@ -0,0 +1,5 @@
+PRETRAINED_COMMENTS_FILE_ID = "1tgMaRFG3C-bbr3uYny8N2R7wlrDwW9Pf"
+PRETRAINED_COMMENTS_FILE_PATH = "data/raw/pretrained_comments.csv"
+
+MOVIES_FILE_PATH = "data/raw/movies"
+NUM_BATCHES = 1000
